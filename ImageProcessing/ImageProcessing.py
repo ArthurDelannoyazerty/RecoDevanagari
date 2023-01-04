3aa2f2523cc62f13f1fbe231febcc8797600ca26
Clement <clement.vivier@reseau.eseo.fr>
2023-01-04 10:16:57 +0100

    update image processing

--- ImageProcessing/ImageProcessing.py
+++ ImageProcessing/ImageProcessing.py
@@ -1,9 +1,14 @@
-from PIL.Image import *
-import cv2 
-dim = (32,32)
-img=cv2.imread("ImageProcessing/Test/IMG_8059.png",0) #image a selection
-ret3,th3 = cv2.threshold(img,240,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-resized = cv2.resize(th3, dim, interpolation = cv2.INTER_AREA)
-cv2.imwrite("ImageProcessing/Result/test.png", resized)
+import cv2
+import numpy as np
 
+src = cv2.imread("ImageProcessing/Test/IMG_8065.png")
 
+bg = cv2.dilate(src, np.ones((5,5), dtype=np.uint8))
+bg = cv2.GaussianBlur(bg, (5,5), 1)
+src_no_bg = 255 - cv2.absdiff(src, bg)
+maxValue = 255
+thresh = 160
+dim = (src.shape[1] - 15, src.shape[0] - 100 )
+retval, dst = cv2.threshold(src_no_bg, thresh, maxValue, cv2.THRESH_BINARY_INV)
+ret3,th3 = cv2.threshold(dst,thresh,maxValue,cv2.THRESH_BINARY)
+cv2.imwrite("ImageProcessing/Result/test.png", th3)
