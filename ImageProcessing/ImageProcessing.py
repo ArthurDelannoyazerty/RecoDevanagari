2842182c2f0c71a4078278782dc52aa89e385ef1
Clement <clement.vivier@reseau.eseo.fr>
2023-01-04 14:47:02 +0100

    update image process

--- ImageProcessing/ImageProcessing.py
+++ ImageProcessing/ImageProcessing.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pytesseract
 
 src = cv2.imread("ImageProcessing/Test/IMG_8065.png")
 bg = cv2.dilate(src, np.ones((5,5), dtype=np.uint8))
@@ -13,9 +14,6 @@ dim = (src.shape[1] - 15, src.shape[0] - 100 )
 retval, dst = cv2.threshold(gray1, thresh, maxValue, cv2.THRESH_BINARY_INV)
 ret3,th3 = cv2.threshold(dst,thresh,maxValue,cv2.THRESH_BINARY)
 
-# contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# img_contours = np.zeros(src.shape)
-# cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
 
 rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 dilation = cv2.dilate(th3, rect_kernel, iterations = 1)
@@ -23,15 +21,27 @@ contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
                                                  cv2.CHAIN_APPROX_NONE)
 im2 = th3.copy()
 i = 0
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
-     
-    # Drawing a rectangle on copied image
-    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 1)
-    cropped = im2[y:y + h, x:x + w]
-    cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_"+str(i)+".png",cropped)
-    i = i+1
-    # Open the file in append mode     
-    # Apply OCR on the cropped image
+for contour in contours:
+    # Find the bounding box of the contour
+    x, y, w, h = cv2.boundingRect(contour)
+    
+    # Check if the bounding box is too small, indicating that the character is joined to another character
+    # if w > 130 or h > 130:
+    #     crop_image1 = im2[y:y+h, x:x+(w//2)]
+    #     crop_image2 = im2[y:y+h, x+(w//2):x+w]
+    #     cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_{}_1.png".format(i), crop_image1)
+    #     cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_{}_2.png".format(i), crop_image2)
+    #     continue
+    # else :
+    # Draw the bounding box on the image
+    cv2.rectangle(im2, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    
+    # Crop the image
+    crop_image = im2[y:y+h, x:x+w]
+    
+    # Save the cropped image to a file
+    cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_{}.png".format(i), crop_image)
+    
+    i += 1
 
 cv2.imwrite("ImageProcessing/Result/test.png", im2)
