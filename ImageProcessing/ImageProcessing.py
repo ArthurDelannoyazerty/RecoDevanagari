0a277a74b51817546cecf0f7ffd448ffabfa81fb
Clement <clement.vivier@reseau.eseo.fr>
2023-01-05 10:08:24 +0100

    cropped image only cara and contour on result

--- ImageProcessing/ImageProcessing.py
+++ ImageProcessing/ImageProcessing.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import pytesseract
 
-src = cv2.imread("ImageProcessing/Test/IMG_8065.png")
+src = cv2.imread("ImageProcessing/Test/DevanagariLetters.png")
 bg = cv2.dilate(src, np.ones((5,5), dtype=np.uint8))
 bg = cv2.GaussianBlur(bg, (5,5), 1)
 src_no_bg = 255 - cv2.absdiff(src, bg)
@@ -25,23 +25,15 @@ for contour in contours:
     # Find the bounding box of the contour
     x, y, w, h = cv2.boundingRect(contour)
     
-    # Check if the bounding box is too small, indicating that the character is joined to another character
-    # if w > 130 or h > 130:
-    #     crop_image1 = im2[y:y+h, x:x+(w//2)]
-    #     crop_image2 = im2[y:y+h, x+(w//2):x+w]
-    #     cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_{}_1.png".format(i), crop_image1)
-    #     cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_{}_2.png".format(i), crop_image2)
-    #     continue
-    # else :
-    # Draw the bounding box on the image
-    cv2.rectangle(im2, (x, y), (x + w, y + h), (255, 0, 0), 2)
     
     # Crop the image
     crop_image = im2[y:y+h, x:x+w]
     
     # Save the cropped image to a file
     cv2.imwrite("ImageProcessing/Result/ImageCropped/Cropped_{}.png".format(i), crop_image)
-    
+        
+    cv2.rectangle(im2, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
     i += 1
 
 cv2.imwrite("ImageProcessing/Result/test.png", im2)
